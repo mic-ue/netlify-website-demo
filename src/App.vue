eff86e17c0b0cfda2157e6e61809374036746c7d
mic-ue <sikapogee@gmail.com>
2020-10-21 22:41:23 +0900

    add iroiro font settings

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <font-awesome-icon icon="birthday-cake" class="icon"/>
+    <font-awesome-icon icon="birthday-cake" class="icon"/>
+    <font-awesome-icon icon="birthday-cake" class="icon"/>
+    <font-awesome-icon icon="birthday-cake" class="icon"/>
+    <font-awesome-icon icon="birthday-cake" class="icon"/>
+    <font-awesome-icon icon="birthday-cake" class="icon"/>
+    <font-awesome-icon :icon="['fab','github']" class="icon"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import 'normalize.css'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -17,12 +25,21 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'M PLUS 1p', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.icon {
+  margin-right: 10px;
+  font-size: 2em;
+}
+
 </style>
