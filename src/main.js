7e209fce60477d66e031169b43a42aff2fbc72e1
mic-ue <sikapogee@gmail.com>
2020-10-27 03:09:33 +0900

    bootstrapを追加

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGlobe,faBlog,faBirthdayCake,faImage } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarAlt,faBook,faHatWizard,faBirthdayCake,faBroom,faFire} from '@fortawesome/free-solid-svg-icons'
 import { faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faGlobe,faBlog,faBirthdayCake,faImage,faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare)
+library.add(faCalendarAlt,faBook,faHatWizard,faBirthdayCake,faBroom,faFire,faTwitter,faGithub,faHtml5,faCss3Alt,faJsSquare)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 import VueTyperPlugin from 'vue-typer'
