08b765e731f98e8efa02029392c2dd2c0b38db11
mic-ue <sikapogee@gmail.com>
2020-10-26 23:34:45 +0900

    トップページを追加

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div id="app">
-    <vue-particles
-      color="#dedede"
-      :particleOpacity="0.7"
-      :particlesNumber="100"
-      shapeType="circle"
-      :particleSize="4"
-      linesColor="#dedede"
-      :linesWidth="1"
-      :lineLinked="true"
-      :lineOpacity="0.4"
-      :linesDistance="150"
-      :moveSpeed="1"
-      :hoverEffect="true"
-      hoverMode="bubble"
-      :clickEffect="true"
-      clickMode="push"
-    >
-    </vue-particles>
-    <vue-typer :text="['welcome this web site\nthis is my schedule calender\n探し物は見つかりそうですか？\n\n\n']"
-    class="top-text" :repeat="0" :preTypeDelay="1000" :typeDelay="120" :preEraseDelay="5000" :eraseDelay="2000"></vue-typer><br>
-
+    <TopPage />
     <font-awesome-icon icon="birthday-cake" class="icon"/>
     <font-awesome-icon icon="birthday-cake" class="icon"/>
     <font-awesome-icon icon="birthday-cake" class="icon"/>
     <font-awesome-icon icon="birthday-cake" class="icon"/>
     <font-awesome-icon icon="birthday-cake" class="icon"/>
-    <font-awesome-icon icon="birthday-cake" class="icon"/>
+    
     <font-awesome-icon :icon="['fab','github']" class="icon"/>
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -36,34 +16,48 @@
 <script>
 import 'normalize.css'
 import HelloWorld from './components/HelloWorld.vue'
+import TopPage from './components/TopPage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TopPage,
   }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@500&display=swap');
+
+html, body, #app {
+  height: 100%;
+}
 
 #app {
+  position: relative;
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   font-family: 'M PLUS 1p', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color:royalblue;
+  color: #0092ca;
   margin-top: 60px;
 
-  background-color: black;
+  background-color: gray;
+}
+
+#main {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
 }
 .icon {
   margin-right: 10px;
-  font-size: 2em;
+  font-size: 1.5em;
 }
 .vue-typer .custom.char {
-  color:royalblue;
+  color: #0092ca;
 }
 </style>
---
+++ src/components/TopPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="top">
+    <div class="particles">
+      <vue-particles
+        color="#dedede"
+        :particleOpacity="0.5"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#eee"
+        :linesWidth="0.5"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="push"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+    </div>
+
+    <div class="contents">
+      <vue-typer :text="['TWST GOODS CALENDAR\n']"
+        class="top-text" :repeat="0" :preTypeDelay="1000" :typeDelay="120" :preEraseDelay="5000" :eraseDelay="2000">
+      </vue-typer>
+      <br>
+
+      <font-awesome-icon icon="calendar-alt" class="icon"/>
+      <font-awesome-icon icon="book" class="icon"/>
+      <font-awesome-icon icon="hat-wizard" class="icon"/>
+      <font-awesome-icon icon="broom" class="icon"/>
+      <font-awesome-icon icon="birthday-cake" class="icon"/>
+      <font-awesome-icon icon="fire" class="icon"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Top",
+}
+</script>
+
+<style scoped>
+.top {
+  width: 100vw;
+  height: 70vw;
+  position: relative; 
+  background-color:#222831;
+}
+.particles {
+  display: flex;
+  height: 100%;
+}
+.contents {
+  position: absolute;
+  top: 40%;
+  left: 25%;
+  transform: translateY(-50%) translateX(-50%);
+}
+</style>
